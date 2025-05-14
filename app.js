fc157dba079cc32ef2d613ccc5b2940cbe73d0b3
muneraisa <muneeraisa33@gmail.com>
2025-05-14 20:04:32 +0300

    images and tiles in the puzzle grid

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const puzzle = document.getElementById("puzzle");
-const movesEl = document.getElementById("moves");
-const timerEl = document.getElementById("timer");
 
 
+const correctOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+window.onload = function () {    //once loaded, bring the tiles
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) { //loop through the grid
+      
+      let tile = document.createElement("img"); //create the elemnts from here instead of having it static in the HTML
+      tile.id = r.toString() + "-" + c.toString(); //pointer to the place of the tile, its like a matrix :)
+      tile.src = "./images/" + correctOrder.shift() + ".jpg"; // the path
+
+      
+      document.getElementById("puzzle").append(tile); // add to the puzzle div
+    }
+  }
+};
