457f12a6527f49354bf979e7ba91147e08d35bd1
muneraisa <muneeraisa33@gmail.com>
2025-05-21 19:44:18 +0300

    drag listeners

--- app.js
+++ app.js
@@ -17,12 +17,46 @@ window.onload = function () {
       tile.id = r.toString() + "-" + c.toString(); //pointer to the place of the tile, its like a matrix :)
       tile.src = "./images/" + shuffled.shift() + ".jpg"; // the path
 
-      
+      //DRAG FUNCTIONALITY
+      tile.addEventListener("dragstart", dragStart); //click an image to drag
+      tile.addEventListener("dragover", dragOver); //moving image around while clicked
+      tile.addEventListener("dragenter", dragEnter); //dragging image onto another one
+      // tile.addEventListener("dragleave", dragLeave); //dragged image leaving anohter image
+      tile.addEventListener("drop", dragDrop); //drag an image over another image, drop the image
+      tile.addEventListener("dragend", dragEnd); //after drag drop, swap the two tiles
 
       //insert the tile to the board of the puzzle
       document.getElementById("puzzle").append(tile); // add to the puzzle div
     }
   }
+};
+
+function dragStart() {
+  currentTile = this; //the tile i am picking
 }
 
+function dragOver(e) {
+  e.preventDefault();
+}
 
+function dragEnter(e) {
+  e.preventDefault();
+}
+
+function dragLeave() {}
+
+function dragDrop() {
+  blankTile = this; //the tile i am droping on
+}
+
+//swap conditions
+function dragEnd() {
+  let holding = currentTile.src;
+  let dropOn = blankTile.src;
+
+  //swap tiles
+  blankTile.src = holding;
+  currentTile.src = dropOn;
+
+  //the coordinates are 0-0, 1-1 with dashes ...
+}
