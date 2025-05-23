c1a348e59612684109405a78f106afc60f407180
muneraisa <muneeraisa33@gmail.com>
2025-05-23 18:22:44 +0300

    removed comments

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let moves = 0;
 const correctOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function startGame() {
-  //clear last game :
+ 
   moves = 0;
   document.getElementById("puzzle").innerHTML = "";
   document.getElementById("message").innerHTML = "";
@@ -17,24 +17,22 @@ function startGame() {
 
   const shuffled = ["4", "2", "8", "5", "1", "6", "7", "9", "3"];
 
-  //once loaded, create the tiles
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < columns; c++) {
-      //loop through the grid
 
-      let tile = document.createElement("img"); //create the elemnts from here instead of having it static in the HTML
-      tile.setAttribute("id", r.toString() + "," + c.toString()); // assign coordinate ids
-      tile.src = "./images/" + shuffled.shift() + ".jpg"; // the path
+      let tile = document.createElement("img"); 
+      tile.setAttribute("id", r.toString() + "," + c.toString()); 
+      tile.src = "./images/" + shuffled.shift() + ".jpg"; 
 
-      //DRAG FUNCTIONALITY
-      tile.addEventListener("dragstart", dragStart); //click an image to drag
-      tile.addEventListener("dragover", dragOver); //moving image around while clicked
-      tile.addEventListener("dragenter", dragEnter); //dragging image onto another one
-      tile.addEventListener("drop", dragDrop); //drag an image over another image, drop the image
-      tile.addEventListener("dragend", dragEnd); //after drag drop, swap the two tiles
+      
+      tile.addEventListener("dragstart", dragStart); 
+      tile.addEventListener("dragover", dragOver); 
+      tile.addEventListener("dragenter", dragEnter);
+      tile.addEventListener("drop", dragDrop); 
+      tile.addEventListener("dragend", dragEnd); 
 
-      //insert the tile to the board of the puzzle
-      document.getElementById("puzzle").append(tile); // add to the puzzle div
+      
+      document.getElementById("puzzle").append(tile); 
     }
   }
 }
@@ -42,7 +40,7 @@ function startGame() {
 window.onload = startGame;
 
 function dragStart() {
-  currentTile = this; //the tile i am picking
+  currentTile = this; 
 }
 
 function dragOver(e) {
@@ -56,40 +54,34 @@ function dragEnter(e) {
 function dragLeave() {}
 
 function dragDrop() {
-  blankTile = this; //the tile i am droping on
+  blankTile = this; 
 }
 
-//coordinates
+
 function getCoords(tile) {
   let coordinate = tile.id.split(",").map((coord) => parseInt(coord));
   return coordinate;
 }
 
-//swap conditions
+
 function checkAdjacent() {
-  let h = false; // horizontal move
-  let v = false; // veertical
+  let h = false; 
+  let v = false; 
 
   let [r, c] = getCoords(currentTile);
   let [r2, c2] = getCoords(blankTile);
 
-  console.log("before check:", h, v);
-
-  //horizontal left or right
   if (r == r2 && (c + 1 == c2 || c - 1 == c2)) {
     h = true;
   }
 
-  //vertical up or down
   if (c == c2 && (r + 1 == r2 || r - 1 == r2)) {
     v = true;
   }
 
-  console.log("after check:", h, v);
   return h || v;
 }
 
-//win
 function checkWin() {
   let order = [];
   let message = "";
@@ -97,20 +89,16 @@ function checkWin() {
   tiles.forEach((tile) => {
     let src = tile.src;
     let parts = src.split("/");
-    let img = parts[parts.length - 1]; // last part of the til which contain img Num
+    let img = parts[parts.length - 1]; 
     let tileNum = img.split(".")[0];
     order.push(tileNum);
   });
-
-  console.log("Current order:", order.join());
-  console.log("Correct order:", correctOrder.join());
 
   if (order.join() == correctOrder.join()) {
     message = document.createElement("h2");
     message.innerText = "YOU WIN 🎉!";
     document.getElementById("message").append(message);
 
-    //play again
     let againBtn = document.createElement("button");
     againBtn.onclick = startGame;
     againBtn.innerText = "Play Again !";
@@ -118,21 +106,20 @@ function checkWin() {
   }
 }
 
-//loss
+
 function Loss() {
   let message = "";
   message = document.createElement("h2");
   message.innerText = "TOO MANY MOVES, YOU LOSE ☹️!";
   document.getElementById("message").append(message);
 
-  //play again
   let againBtn = document.createElement("button");
   againBtn.onclick = startGame;
   againBtn.innerText = "Play Again !";
   document.getElementById("again").append(againBtn);
 }
 
-//swap once dropped
+
 function dragEnd() {
   if (!blankTile.src.includes("1.jpg")) {
     return;
@@ -142,16 +129,14 @@ function dragEnd() {
     let holding = currentTile.src;
     let dropOn = blankTile.src;
 
-    //swap tiles
     blankTile.src = holding;
     currentTile.src = dropOn;
+   
+    moves = moves + 1;
+    document.getElementById("moves").innerText = moves;
   }
 
-  //moves
-  moves = moves + 1;
-  document.getElementById("moves").innerText = moves;
-
-  if (moves > 20) {
+  if (moves >= 20) {
     Loss();
     return;
   }
